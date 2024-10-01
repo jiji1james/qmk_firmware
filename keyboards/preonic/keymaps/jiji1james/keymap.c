a1c40bcfaa7c5dde4cee3a658c9a3df003e0ebd0
Jiji James <jijijames@crsoftware.com>
2024-10-01 14:21:12 -0400

    Update layers for navigation and added smart space key

--- keyboards/preonic/keymaps/jiji1james/keymap.c
+++ keyboards/preonic/keymaps/jiji1james/keymap.c
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------------+------------+------------+------------+------------+--------|                    |------------+------------+------------+------------+------------+--------|
       KC_TAB,      KC_EXLM,     KC_AT,       KC_HASH,     KC_DLR,      KC_PERC,                      KC_CIRC,     KC_AMPR,     KC_ASTR,     KC_LPRN,     KC_RPRN,     KC_BSPC,
   //|------------+------------+------------+------------+------------+--------|                    |------------+------------+------------+------------+------------+--------|
-      KC_ESC,      KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,    XXXXXXX,                      KC_MINS,     KC_EQL,      KC_LBRC,     KC_RBRC,     KC_BSLS,     KC_GRV,
+      KC_ESC,      KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,    XXXXXXX,                      KC_EQL,      KC_MINS,     KC_LBRC,     KC_RBRC,     KC_BSLS,     KC_GRV,
   //|------------+------------+------------+------------+------------+--------|                    |------------+------------+------------+------------+------------+--------|
-      KC_LSFT,     KC_HOME,     KC_PGDN,     KC_PGUP,     KC_END,      XXXXXXX,                      KC_UNDS,     KC_PLUS,     KC_LCBR,     KC_RCBR,     KC_PIPE,     KC_TILD,
+      KC_LSFT,     KC_HOME,     KC_PGDN,     KC_PGUP,     KC_END,      XXXXXXX,                      KC_PLUS,     KC_UNDS,     KC_LCBR,     KC_RCBR,     KC_PIPE,     KC_TILD,
   //|------------+------------+------------+------------+------------+--------|                    |------------+------------+------------+------------+------------+--------|
       XXXXXXX,     XXXXXXX,     XXXXXXX,     _______,     MO(_THIRD),  _______,                      _______,      _______,     _______,     XXXXXXX,     XXXXXXX,     XXXXXXX
   //|------------+------------+------------+------------+------------+--------|                    |------------+------------+------------+------------+------------+--------|
